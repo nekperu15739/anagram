a4ac6fef4d4a7a969f1d3fcad34f54eb5c81be3d
Chavez, Charly <chavezch@ryanair.com>
2021-05-25 12:17:34 +0200

    fix tests

--- src/test/java/com/nekperu15739/anagram/AnagramApplicationTests.java
+++ src/test/java/com/nekperu15739/anagram/AnagramApplicationTests.java
@@ -4,10 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+    (properties = {
+        "spring.cloud.kubernetes.enabled=false",
+        "spring.cloud.kubernetes.discovery.enabled=false",
+        "spring.cloud.kubernetes.config.enable-api=false",
+        "kubernetes.informer.enabled=false"
+    })
 class AnagramApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
